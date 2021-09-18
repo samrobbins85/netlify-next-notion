ad813cda25fa0893002d57709870d7ce526b529c
Sam Robbins <samrobbinsgb@gmail.com>
2021-09-18 16:55:53 +0100

    ✨ Create new page in notion database

--- netlify/functions/submission-created.js
+++ netlify/functions/submission-created.js
@@ -1,5 +1,39 @@
+const fetch = require("node-fetch");
+const database_id = "dd010e0a312d481e94dbf93d9d81094d";
 exports.handler = async function (event, context) {
   const form = JSON.parse(event.body).payload.data;
+  try {
+    const new_row = {
+      parent: {
+        database_id: database_id,
+      },
+      properties: {
+        Name: {
+          title: [
+            {
+              text: {
+                content: form.name,
+              },
+            },
+          ],
+        },
+      },
+    };
+
+    const response = await fetch("https://api.notion.com/v1/pages", {
+      method: "POST",
+      headers: {
+        "Notion-Version": "2021-08-16",
+        Authorization: `Bearer ${process.env.NOTION_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(new_row),
+    });
+    const data = await response.json();
+    console.log(data);
+  } catch {
+    console.log(error);
+  }
   return {
     statusCode: 200,
     body: "Hi " + form.name,
