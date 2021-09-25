ddff44996b35d2ac0633baf40b9f23a1c61508b4
Sam Robbins <samrobbinsgb@gmail.com>
2021-09-25 10:17:43 +0100

    Tidy up

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,30 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
-    <main>
-      <h1>Netlify Forms to Notion</h1>
-      <form name="contact" method="POST" data-netlify="true">
-        <p>
-          <label>
-            Your Name: <input type="text" name="name" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Email: <input type="email" name="email" />
-          </label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-        <input type="hidden" name="form-name" value="contact" />
-      </form>
-    </main>
+    <>
+      <Head>
+        <title>Netlify Forms to Notion</title>
+      </Head>
+      <main>
+        <h1>Netlify Forms to Notion</h1>
+        <form name="contact" method="POST" data-netlify="true">
+          <p>
+            <label>
+              Your Name: <input type="text" name="name" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your Email: <input type="email" name="email" />
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+          <input type="hidden" name="form-name" value="contact" />
+        </form>
+      </main>
+    </>
   );
 }
