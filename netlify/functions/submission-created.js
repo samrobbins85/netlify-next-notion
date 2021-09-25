fcb8e765635a35a28b32baf65fa95a17cc6f9a3b
Sam Robbins <samrobbinsgb@gmail.com>
2021-09-25 16:31:35 +0100

    Update to use env var for database id

--- netlify/functions/submission-created.js
+++ netlify/functions/submission-created.js
@@ -1,12 +1,11 @@
 const { Client } = require("@notionhq/client");
-const database_id = "dd010e0a312d481e94dbf93d9d81094d";
 exports.handler = async function (event) {
   const form = JSON.parse(event.body).payload.data;
   const notion = new Client({ auth: process.env.NOTION_TOKEN });
   try {
     const new_row = {
       parent: {
-        database_id: database_id,
+        database_id: process.env.DATABASE_ID,
       },
       properties: {
         Name: {
